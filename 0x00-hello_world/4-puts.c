c5471336eab371bf5c6d652dee6611cf2ff50487
zgithub3 <zo5673496@gmail.com>
2022-06-05 08:19:18 +0300

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - entry point
- *
- * return: always 0
- */
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
