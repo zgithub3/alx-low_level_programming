7d2ed5408b550b2625f5e6e1c7783670ac9b6941
zgithub3 <zo5673496@gmail.com>
2022-06-05 08:49:49 +0300

    0x00. C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ float f;
 printf("Size of a char:%d byte(s)\n", sizeof(a));
 printf("Size of an int:%d byte(s)\n", sizeof(b));
 printf("Size of a long int:%d byte(s)\n",sizeof(c));
-printf("size of a long long int:%d byte(s)\n", sizeof(d));
-printf("size of a float:%d byte(s)\n", sizeof(f));
+printf("Size of a long long int:%d byte(s)\n", sizeof(d));
+printf("Size of a float:%d byte(s)\n", sizeof(f));
 return (0);
 }
